1102fa46196f56040e8d41c5795b9d6b30d2a407
natae <y2kmsp@naver.com>
2022-09-11 13:45:40 +0900

    First commit

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get("/", (req: Request, res: Response)=>{
+    res.send("Hello world");
+});
+
+app.listen(8080);
