3c0fb8dedc55ee737ebf7470ae99d59166e69861
Na Taesoo <y2kmsp@naver.com>
2022-09-14 23:55:04 +0900

    remove dist

--- api-server/dist/controller/hello.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
-function hello(req, res) {
-    res.send("Hello World");
-}
-exports.hello = hello;
